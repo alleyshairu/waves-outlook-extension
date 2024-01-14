02e197af2ede30eaa91ff6ebd1d26f453ec3bec0
alleyshairu <tfck.10@gmail.com>
2024-01-14 12:49:09 +1100

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,4 @@ echo "*** Uploading files to server ***"
 scp -r dist igor@170.64.185.82:/home/igor/waves/
 
 echo "*** Restarting caddy container ***"
-ssh -T igor@170.64.185.82 <<'EOL'
-  cd waves
-  cp -R dist/* mail-assistant
-  rm -R rf dist/
-  cd caddy
-  docker compose down
-  docker compose up -d --build
-'EOL
+ssh -T igor@170.64.185.82 "cd waves; cp -R dist/* mail-assistant; rm -R dist/; cd caddy; docker compose down; docker compose up -d --build"
